acca10b2b506e700a80fc811d1f058dfed5150a3
jyow <49602630+caskr@users.noreply.github.com>
2021-02-04 16:41:51 +0800

    Update LC_105.java

--- java/LC_105.java
+++ java/LC_105.java
@@ -14,19 +14,16 @@
  * }
  */
 class Solution {
+    HashMap<Integer,Integer>hmap;
     public TreeNode buildTree(int[] preorder, int[] inorder) {
+        hmap = new HashMap<>();
+        for(int i=0; i<inorder.length; i++) hmap.put(inorder[i],i);
         return dfs(preorder,inorder,0,preorder.length-1,0,inorder.length-1);
     }
     TreeNode dfs(int[] pre, int[] in, int pl, int pr, int il, int ir) {
         if(pr < pl) return null;
         TreeNode now = new TreeNode(pre[pl]);
-        int root = 0;
-        for(int i = il; i <= ir; i++){
-            if(in[i] == pre[pl]){
-                root = i;
-                break;
-            }
-        }
+        int root = hmap.get(pre[pl]);
         now.left = dfs(pre,in,pl+1,pl+root-il,il,root-1);
         now.right = dfs(pre,in,pl+root-il+1,pr,root+1,ir);
         return now;
