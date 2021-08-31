2451cbef0ae7ec7ba359e5adcf9a5627bdf1d97d
jyow <49602630+caskr@users.noreply.github.com>
2021-08-31 20:59:46 +0800

    NC_45非递归

--- java/NC_45.java
+++ java/NC_45.java
@@ -14,8 +14,7 @@ public class Solution {
      * @param root TreeNode类 the root of binary tree
      * @return int整型二维数组
      */
-    /*
-    先序、中序、后序分别一个函数，麻烦
+    
     private List<Integer> first = new ArrayList<>();
     private List<Integer> mid = new ArrayList<>();
     private List<Integer> last = new ArrayList<>();
@@ -23,8 +22,8 @@ public class Solution {
         // write code here
         if(root == null)    return null;
         firstOrder(root);
-        midOrder(root);
         lastOrder(root);
+        midOrder(root);
         int[][] res = new int[3][first.size()];
         for(int i=0; i<first.size(); i++){
             res[0][i] = first.get(i);
@@ -33,52 +32,101 @@ public class Solution {
         }
         return res;
     }
-    public void firstOrder(TreeNode root) {
-        if(root == null)    
+    public void firstOrder (TreeNode root) {
+        if(root == null)
             return;
-        first.add(root.val);
-        if(root.left != null)
-            firstOrder(root.left);
-        if(root.right != null)
-            firstOrder(root.right);
+        Stack<TreeNode> s = new Stack<>();
+        s.push(root);
+        while(!s.isEmpty()) {
+            TreeNode cur = s.pop();
+            first.add(cur.val);
+            if(cur.right!=null)
+                s.push(cur.right);
+            if(cur.left!=null) {
+                s.push(cur.left);
+            }
+        }
     }
-    public void midOrder(TreeNode root) {
-        if(root == null)    
+    public void midOrder (TreeNode root) {
+        if(root == null)
             return;
-        if(root.left != null)
-            midOrder(root.left);
-        mid.add(root.val);
-        if(root.right != null)
-            midOrder(root.right);
+        Stack<TreeNode> s = new Stack<>();
+        s.push(root);
+        while(!s.isEmpty()) {
+            TreeNode cur = s.peek();
+            if(cur.left!=null) {
+                s.push(cur.left);
+                cur.left = null;
+            } else if(cur.right!=null) {
+                s.pop();
+                mid.add(cur.val);
+                s.push(cur.right);
+            } else {
+                s.pop();
+                mid.add(cur.val);
+            }
+        }
     }
-    public void lastOrder(TreeNode root) {
-        if(root == null)    
-            return;
-        if(root.left != null)
-            lastOrder(root.left);
-        if(root.right != null)
-            lastOrder(root.right);
-        last.add(root.val);
+    public void lastOrder (TreeNode root) {
+        Stack<TreeNode> s = new Stack<>();
+        s.push(root);
+        while(!s.isEmpty()) {
+            TreeNode cur = s.pop();
+            if(cur.left!=null) {
+                s.push(cur.left);
+            }
+            if(cur.right!=null) {
+                s.push(cur.right);
+            }
+            last.add(0,cur.val);
+        }
     }
-    */
-    int[][] res;
-    int first=0,mid=0,last=0;
-    public int[][] threeOrders (TreeNode root) {
-        this.res = new int[3][getSize(root)];
-        order(root);
-        return res;
-    }
-    public int getSize(TreeNode root){
-        if(root == null)    return 0;
-        return 1+getSize(root.left)+getSize(root.right);
-    }
-    public void order(TreeNode root){
-        if(root == null)    return;
-        res[0][first++] = root.val;
-        order(root.left);
-        res[1][mid++] = root.val;
-        order(root.right);
-        res[2][last++] = root.val;
-        return;
-    }
+//     public void firstOrder(TreeNode root) {
+//         if(root == null)    
+//             return;
+//         first.add(root.val);
+//         if(root.left != null)
+//             firstOrder(root.left);
+//         if(root.right != null)
+//             firstOrder(root.right);
+//     }
+//     public void midOrder(TreeNode root) {
+//         if(root == null)    
+//             return;
+//         if(root.left != null)
+//             midOrder(root.left);
+//         mid.add(root.val);
+//         if(root.right != null)
+//             midOrder(root.right);
+//     }
+//     public void lastOrder(TreeNode root) {
+//         if(root == null)    
+//             return;
+//         if(root.left != null)
+//             lastOrder(root.left);
+//         if(root.right != null)
+//             lastOrder(root.right);
+//         last.add(root.val);
+//     }
+   
+//     int[][] res;
+//     int first=0,mid=0,last=0;
+//     public int[][] threeOrders (TreeNode root) {
+//         this.res = new int[3][getSize(root)];
+//         order(root);
+//         return res;
+//     }
+//     public int getSize(TreeNode root){
+//         if(root == null)    return 0;
+//         return 1+getSize(root.left)+getSize(root.right);
+//     }
+//     public void order(TreeNode root){
+//         if(root == null)    return;
+//         res[0][first++] = root.val;
+//         order(root.left);
+//         res[1][mid++] = root.val;
+//         order(root.right);
+//         res[2][last++] = root.val;
+//         return;
+//     }
 }
